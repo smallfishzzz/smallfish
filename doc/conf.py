7541118387374ac4005326c3197b15f4cd1c7401
fish-wsl <316120728@qq.com>
2023-06-14 13:26:15 +0800

    add localtoc

--- doc/conf.py
+++ doc/conf.py
@@ -118,7 +118,7 @@ html_sidebars = {
         'searchbox.html',
         'navigation.html',
         'relations.html',  # needs 'show_related': True theme option to display
-        #'localtoc.html',
+        'localtoc.html',
         #'globaltoc.html',
     ]
 }
